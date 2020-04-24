40aaba70cd7eb4619c476dffa76e9b1cbac040b3
mya123123 <63846706+mya123123@users.noreply.github.com>
2020-04-24 13:46:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <?php 
-    include("index.html");
-    ?>
+    <h1>An dau buoi, an cut</h1>
+    <p>Lieu thi an nhieu, ko lieu thi an it</p>
+    <p>Khong lam ma doi an thi an duoi buoi, an cut</p>
 </body>
+
 </html>
